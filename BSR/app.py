2263a7e16929555c99a4bea1f484bbe05500ade2
Shuzo14 <shubham.madhavi@loantap.in>
2022-11-30 17:17:33 +0530

    update

--- BSR/app.py
+++ BSR/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-logging.basicConfig(filename='/tmp/record.log', level=logging.DEBUG)
+logging.basicConfig(filename = '/tmp/record.log', level = logging.DEBUG)
 
 def main():
   # showing different logging levels
@@ -19,7 +19,7 @@ def main():
   app.logger.critical("Critical log info")
   return "testing logging levels."
 
-app.add_url_rule('/ocr/help',methods = ['GET'], view_func=routes.help)
-app.add_url_rule('/ocr/bankStatement', methods=['POST'], view_func=routes.bankStatement)
+app.add_url_rule('/ocr/help', methods = ['GET'], view_func = routes.help)
+app.add_url_rule('/ocr/bankStatement', methods = ['POST'], view_func = routes.bankStatement)
 
-if __name__ == '__main__': app.run(debug=True)
+if __name__ == '__main__': app.run(debug = True)
